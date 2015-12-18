b2799c1a73621c23f08ed0af8f270fc4cb009967
Sergey Korneev <adsliquid@gmail.com>
2015-12-18 19:31:40 +0300

    upd

--- app/js/main.js
+++ app/js/main.js
@@ -20,23 +20,33 @@ $(document).ready(
                 // Prevents the default action to be triggered. 
                 e.preventDefault();
 
-               
-               // {
-               //  	onClose: function() {
-               //  		$(".tooltip").css({"": ""});
-               //  	}
-               //  }
+
+              
 
 
                 // Triggering bPopup when click event is fired
                 $('#element_to_pop_up').bPopup();
 
             });
-            $('.close-button-link').on('click', function(e) {
+            // $('.close-button-link').on('click', function(e) {
+            // 	e.preventDefault();
+            // 	$('#element_to_pop_up').bPopup().close();
+            // }
+            // 	);
+        $('.close-button-link').on('click', function(e) {
             	e.preventDefault();
-            	$('#element_to_pop_up').bPopup().close();
+              $('#element_to_pop_up').bPopup().close();
+              $(':input', '#jsValidate')
+    						.not(':button, :submit, :reset, :hidden')
+    						.val('')
+    					$('input, textarea').removeClass('tooltip')
+    					$('#add_title-error, #filename-error, #add-url-error, #add-desc-error').css({'display': 'none'})
+             
             }
-            	);
+            	 
+            
+    );
+       
 
         });
 
@@ -51,16 +61,26 @@ $(document).ready( function() {
 });
 
 //Валидация формы добавления проекта с выводом тултипов
-	$("#jsValidate").validate({
-			errorElement: "div",
-			errorClass: "tooltip",
-		messages: {
-			add_title: "Укажите название проекта",
-			project_url: "Укажите URL проекта",
-			project_desc: "Напишите краткое описание проекта",
-			project_file: "Добавьте изображение проекта"
-		}
+  
+$('.add-project-button').on('click', function(e) {
+ 	// e.preventDefault();
+ 	var isValid = $("#jsValidate").validate({
+					errorElement: "div",
+					errorClass: "tooltip",
+					messages: {
+					add_title: "Укажите название проекта",
+					project_url: "Укажите URL проекта",
+					project_desc: "Напишите краткое описание проекта",
+					project_file: "Добавьте изображение проекта"
+				}
+				
 		});
+ 	// if (!isValid == true) {
+ 	// 	$("#success").css({"display": "block"});
+ 	// }
+})
+
+
 // Валидация формы фидбеков с выводом тултипов
 
 
